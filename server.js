8315a55c8ac764a1f003b1e29f46ef84e18f394d
kenshii <danny.vt.cao@gmail.com>
2022-01-09 12:31:42 +1100

    able to render html files, issue with clicking on index hyperlink

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const http = require('http')
 const fs = require('fs')
-
+const { URL } = require('url')
+const { url } = require('inspector')
+const baseDir = __dirname
 const hostname = '127.0.0.1'
 const port = process.env.port || 8080
 
 // const index = fs.readFile('./index.html');
+const validUrls = [`/`, `about`, `contact-me`,]
+
 
 const server = http.createServer((req, res) => {
-  const filename=__dirname+req.url
-  console.log(port)
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text-html')
-  res.end(`${filename}`);
+  try {
+    let html;
+    if(req.url === '/') html = './index.html'
+    html = `.${req.url}.html`
 
+    // if(req.url === '/') filepath = `${baseDir}index.html`
+    fs.readFile(html, (err, data) => {
+      res.writeHeader(200, {'Content-Type': 'text/html', 'Content-Length': data.length})
+      res.write(data)
+      res.end()
+    })
+
+  } catch(err) {
+    res.writeHead(500)
+    res.end()
+    console.log(err.stack)
+  }
 })
 
 
