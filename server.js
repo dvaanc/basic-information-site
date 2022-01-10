4762ec79bab3b994b67a21d6a441c4a487cae841
kenshii <danny.vt.cao@gmail.com>
2022-01-10 17:02:01 +1100

    finished

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
 const http = require('http')
-const fs = require('fs')
+const { readFile } = require('fs')
 const { URL } = require('url')
-const { url } = require('inspector')
 const baseDir = __dirname
-const hostname = '127.0.0.1'
+const hostname = process.env.hostname || 'localhost'
 const port = process.env.port || 8080
 
-// const index = fs.readFile('./index.html');
-const validUrls = [`/`, `about`, `contact-me`,]
-
-
 const server = http.createServer((req, res) => {
-  try {
-    let html;
-    if(req.url === '/') html = './index.html'
-    html = `.${req.url}.html`
-
-    // if(req.url === '/') filepath = `${baseDir}index.html`
-    fs.readFile(html, (err, data) => {
-      res.writeHeader(200, {'Content-Type': 'text/html', 'Content-Length': data.length})
-      res.write(data)
-      res.end()
-    })
-
-  } catch(err) {
-    res.writeHead(500)
-    res.end()
-    console.log(err.stack)
+  res.setHeader("Content-Type", "text/html")
+  let path = './'
+  switch(req.url) {
+    case '/':
+      path += 'index.html'
+      res.statusCode = 200
+      break
+    case '/about':
+      path += 'about.html'
+      res.statusCode = 200
+      break
+    case '/contact-me':
+      path += 'contact-me.html'
+      res.statusCode = 200
+      break
+    default:
+      path += '404.html'
+      res.statusCode = 404
+      break
   }
+  readFile(path, (err, data) => {
+    if(err) console.error(err)
+    res.write(data)
+    res.end()
+  })
 })
 
 
 server.listen(port, () => {
-  console.log(`Server running at http://${port}`) 
+  console.log(`Server running at http://${port}`)
 })
+
+server.on('exit', () => server.exit())
+server.on('SIGINT', () => server.exit())
 
